66e06f5598fb2c1a00ccd9c8bfa063d53e3980c9
Kelvin Patrício de Macêdo Santos <30604716+Kelvinpms@users.noreply.github.com>
2023-02-16 15:25:00 -0300

    Create cript_de_envio_get_e_post.py

---
+++ cript_de_envio_get_e_post.py
@@ -0,0 +1,62 @@
+import psutil
+import time
+import platform
+import requests
+from requests.auth import HTTPBasicAuth
+
+auth = HTTPBasicAuth('ServerMonitorUsername', 'ServerMonitorPassword')
+
+contador = 1
+while True:
+    cpu_count = psutil.cpu_count()
+    cpu_load_percent = psutil.cpu_percent(interval=1, percpu=True)
+    mem = psutil.virtual_memory()
+    mem_total = mem.total
+    mem_available = mem.available
+    disk_partitions = psutil.disk_partitions()
+    disk_usage = {
+        partition.device: psutil.disk_usage(partition.mountpoint)._asdict()
+        for partition in disk_partitions
+    }
+    data_services = {
+        "data_services": []
+    }
+    services = psutil.win_service_iter()
+    for service in services:
+        service_data = {
+            "name": service.name(),
+            "display_name": service.display_name(),
+            "status": service.status()
+            # adicione outras informações que você precisar
+        }
+        data_services["data_services"].append(service_data)
+    
+    data = {
+        "monitoring_id": contador,
+        "machine_id": "my_machine",
+        "number_of_logical_processors": cpu_count,
+        "load_percentage": cpu_load_percent[-1],
+        "number_of_cores": psutil.cpu_count(logical=False),
+        "last_boot_time": psutil.boot_time(),
+        "enable_daylight_savings_time": time.localtime().tm_isdst,
+        "total_visible_memory_size": mem_total,
+        "free_physical_memory": mem_available,
+        "drives": disk_usage,
+        "services": data_services,
+        "monitoring_date": time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()),
+        "ip_address": psutil.net_if_addrs()["Ethernet"][1].address,
+        "os_version": platform.platform(),
+    }
+    url = "http://c2aazisyda02:40000/api/Monitoring/Monitor/Create"
+    try:
+        response1 = requests.get(url, json=data, auth=auth)
+        print(response1.text)
+    except:
+        print("get erro") 
+    try:       
+        response = requests.post(url, json=data, auth=auth)
+        print(response.text)
+    except:
+        print("post erro")    
+    contador += 1
+    input()

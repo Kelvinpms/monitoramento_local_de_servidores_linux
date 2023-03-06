8424c2d605d5ec8e8c5474e1a15016c1a7f7800b
Kelvin Patrício de Macêdo Santos <30604716+Kelvinpms@users.noreply.github.com>
2023-03-06 12:05:26 -0300

    Update script.py

--- script.py
+++ script.py
@@ -3,10 +3,10 @@ import psutil
 def monitor():
     # Conectando ao banco de dados PostgreSQL
     conn = psycopg2.connect(
-        host="10.154.211.57S",
-        database="server_monitor",
-        user="postgres",
-        password="postgres"
+        host="",
+        database="",
+        user="",
+        password=""
     )
     # Criando cursor para realizar consultas no banco de dados
     cur = conn.cursor()
